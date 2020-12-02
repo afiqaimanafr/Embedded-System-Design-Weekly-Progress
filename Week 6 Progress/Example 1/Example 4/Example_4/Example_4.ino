ecf52a3c447a27bac1fed50dbb96591bc9a8db23
Afiq Aiman <73816876+afiqaimanafr@users.noreply.github.com>
2020-12-02 16:48:19 +0800

    Add files via upload

---
+++ Week 6 Progress/Example 1/Example 4/Example_4/Example_4.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  unsigned char* ddrb = (unsigned char*) 0x24;
+  unsigned char* ocr1al = (unsigned char*) 0x88;
+  unsigned char* tccr1a = (unsigned char*) 0x80;
+  unsigned char* tccr1b = (unsigned char*) 0x81;
+  *ddrb = 2;
+  *ocr1al = 255;
+  *tccr1a = 0b10000011;
+  *tccr1b = 0b00000010;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
