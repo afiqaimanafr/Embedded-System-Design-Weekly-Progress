65c739cd47a9337cf4e8b36fb2fddef8fff5bbc3
Afiq Aiman <73816876+afiqaimanafr@users.noreply.github.com>
2021-01-14 10:25:11 +0800

    Add files via upload

---
+++ Exercise Using Ultrasonic Sensor and LCD/Ultrasonic_with_LCD/Ultrasonic_with_LCD.ino
@@ -0,0 +1,99 @@
+#include <Wire.h> // Library for I2C communication
+
+#include <LiquidCrystal_I2C.h> // Library for LCD
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,16,2) for 16x2 LCD.my address is (0x3F)
+
+
+
+int trigPin = 9;
+
+int echoPin = 10;
+
+long distance;
+
+long distanceInch;
+
+long duration;
+
+ 
+
+void setup(){
+
+  lcd.init();
+
+  lcd.backlight();
+
+  
+
+ pinMode(trigPin, OUTPUT);
+
+ pinMode(echoPin, INPUT);
+
+
+
+ lcd.setCursor(0,0);
+
+ lcd.print("   WELCOME TO");
+
+ lcd.setCursor(0,1);
+
+ lcd.print("  MKINVENTIONS");
+
+ delay(2000);
+
+}
+
+ 
+
+void loop() {
+
+  ULTRASONIC();
+
+ lcd.clear();
+
+ lcd.setCursor(0,0);
+
+ lcd.print("DISTANCE CM:");
+
+ lcd.print(distance); 
+
+ lcd.setCursor(0,1);
+
+ lcd.print("DISTANCE INCH:");
+
+ lcd.print(distanceInch); 
+
+// lcd.setCursor(0,1);
+
+// lcd.print("DISTANCE MM:");
+
+// lcd.print(distanceInch); 
+
+}
+
+
+
+ 
+
+void ULTRASONIC(){
+
+  digitalWrite(trigPin, LOW);
+
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration*0.034/2;
+
+  distanceInch = duration*0.0133/2;
+
+
+
+  } 
